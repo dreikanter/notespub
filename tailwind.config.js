37f898996fa89d2224a62a105b4f6f1cd5dad130
Alex Musayev <alex.musayev@gmail.com>
2026-03-30 09:17:10 +0200

    Add HTML templates, feed template, and Tailwind CSS source

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./templates/**/*.xml",
+  ],
+  safelist: [
+    "bg-slate-100",
+    "bg-slate-500",
+    "block",
+    "border",
+    "border-slate-200",
+    "border-slate-600",
+    "font-normal",
+    "no-underline",
+    "rounded-md",
+    "text-slate-100",
+    "text-slate-400",
+    "text-slate-800",
+    {
+      "pattern": /m(l|r|x|y)-.+/,
+    },
+    {
+      "pattern": /p(l|r|x|y)-.+/,
+    },
+  ],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            }
+          }
+        }
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
